be77e9d06583b1292f75bc7bc855efb5ffbe6822
Nataliya <nat.georgieva2017@yandex.ru>
2022-09-26 19:01:13 +0300

    Решила вторую задачу

--- Python/task4.1.py
+++ Python/task4.1.py
@@ -1,3 +1,15 @@
 #Вычислить число c заданной точностью d
 #Пример:
 #- при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+
+
+
+import math
+from math import pi
+
+n = int(input('Введите число: '))
+n = pi
+print(n)
+#some_pi = sum(1/16**x*(4/(8*x + 1) - 2/(8*x + 4) - 1/(8*x + 5) - 1/(8*x + 6)) for x in range(n))
+#print(some_pi)
+
