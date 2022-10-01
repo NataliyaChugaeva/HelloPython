1cd0f8632d05f9296839cb80eb1cff186383eecc
Nataliya <nat.georgieva2017@yandex.ru>
2022-10-01 18:03:43 +0300

    Решила другим способом

--- Python/task4.2.py
+++ Python/task4.2.py
@@ -1,6 +1,9 @@
 #Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
 number = int(input("Введите целое число: "))
-for i in range(1, number+1):
-    if(number % i == 0):
-        print(i)
+#for i in range(1, number+1):
+#    if(number % i == 0):
+#        print(i)
+data = [x for x in range(1, number + 1)]
+res = (list(filter(lambda x: number % x == 0,data)))
+print(res)
