2d548f47914b03755d77a7300bc28d6036d8acb7
Nataliya <nat.georgieva2017@yandex.ru>
2022-10-19 22:20:17 +0300

    Добавила файлы

---
+++ Seminar9task1/main.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import random
+
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, User
+from telegram.ext import Updater, CommandHandler, MessageHandler, \
+    ConversationHandler, Filters, CallbackQueryHandler
+
+import str as st
+
+
+
+def start(update, context):
+    context.bot.send_message(
+        update.effective_chat.id,
+        f"Привет! Я бот-калькулятор\n"
+        f"/calc - сделать расчет\n"
+        f"/convert - перевести доллары в рубли")
+
+
+def calc(update, context):
+    context.bot.send_message(
+        update.effective_chat.id,
+        f"Введите выражение\n Для выхода нажмите /stop")
+    return 1
+
+
+def eval_calc(update, context):
+    context.bot.send_message(
+        update.effective_chat.id,
+        f"Результат: {eval(update.message.text)}")
+
+
+def convert(update, context):
+    context.bot.send_message(
+        update.effective_chat.id,
+        f"Введите сумму в $\n Для выхода нажмите /stop")
+    return 1
+
+
+def converter(update, context):
+    context.bot.send_message(
+        update.effective_chat.id,
+        f"Результат: {int(update.message.text) * 55.3}")
+
+
+def stop(update, context):
+    return update.message.reply_text(st.ANSW_STOP), ConversationHandler.END
+
+
+calc_handler = ConversationHandler(
+    entry_points=[CommandHandler('calc', calc)],
+    states={
+        1: [MessageHandler(Filters.text & ~Filters.command, eval_calc)],
+    },
+    fallbacks=[CommandHandler('stop', stop)]
+)
+convert_handler = ConversationHandler(
+    entry_points=[CommandHandler('convert', convert)],
+    states={
+        1: [MessageHandler(Filters.text & ~Filters.command, converter)],
+    },
+    fallbacks=[CommandHandler('stop', stop)]
+)
+
+start_handler = CommandHandler('start', start)
---
+++ Seminar9task1/str.py
@@ -0,0 +1,18 @@
+BOT_TOKEN_FILENAME = 'token.txt'
+
+SYMBOL_X = '❌'
+SYMBOL_O = '⭕'
+SYMBOL_UNDEF = '◻'
+
+
+ANSW_YOUR_TURN = 'Ваш ход'
+ANSW_YOU_WIN = 'Вы победили'
+ANSW_BOT_WIN = 'Победил бот'
+ANSW_DRAW = 'Ничья'
+ANSW_HELP = 'Бот для игры в крестики-нолики\n' \
+            'Используйте команду /new_game для начала новой игры'\
+            'Для остановки напишите команду /stop'
+ANSW_STOP = 'Игра остановлена'
+
+
+ALERT_CANNOT_MOVE_TO_THIS_CELL = 'Нажимать можно только на ' + SYMBOL_UNDEF
