26b5b69ae2e09f791e86a02e365375812a03be77
Nataliya <nat.georgieva2017@yandex.ru>
2022-10-20 18:42:06 +0300

    Добавила решения

---
+++ Seminar10task3/hw.py
@@ -0,0 +1,35 @@
+#Дан текст: в первой строке записано число строк, далее идут сами строки.
+#Определите, сколько различных слов содержится в этом тексте.
+#Словом считается последовательность непробельных символов идущих подряд, 
+#слова разделены одним или большим числом пробелов или символами конца строки.
+#Sample Input:
+#4
+#She sells sea shells on the sea shore;
+#The shells that she sells are sea shells I'm sure.
+#So if she sells sea shells on the sea shore,
+#I'm sure that the shells are sea shore shells.
+#Sample Output:
+#19
+
+
+
+n = int(input())
+A = set()
+for i in range(n):
+    S = input().split()
+    for elem in S:
+        A.add(elem)
+print(len(A))
+
+
+
+
+
+
+
+
+
+
+
+
+
